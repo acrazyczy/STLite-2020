980bd94064736271da4de1d9951d3416f28eb800
acrazyczy <a_crazy_czy@yahoo.com>
2020-04-07 01:09:31 +0800

    Coding RBTree.

--- mapA/map.hpp
+++ mapA/map.hpp
@@ -10,6 +10,13 @@
 #include "utility.hpp"
 #include "exceptions.hpp"
 
+template <class T>
+void swap(T &x , T &y)
+{
+	T z(x);
+	x = y , y = z;
+}
+
 namespace sjtu {
 
 template<
@@ -17,6 +24,182 @@ template<
 	class T,
 	class Compare = std::less<Key>
 > class map {
+private:
+	enum color_type {RED , BLACK};
+
+	struct Node
+	{
+		Key key;
+		T value;
+		color_type color;
+		Node *left , *right , *parent;
+
+		Node (const Key &key_ , const T &value_) : key(key_) , value(value_) , color(RED) , left(nullptr) , right(nullptr) , parent(nullptr) {}
+	}*nil;
+
+	Compare compare;
+	size_t tot;
+
+	void left_rotate(Node *x)
+	{
+		Node *y = x -> parent;
+		if (x -> left != nil) x -> left -> parent = y;
+		y -> right = x -> left;
+		if (y -> parent -> left == y) y -> parent -> left = x;
+		else y -> parent -> right = x;
+		x -> parent = y -> parent , y -> parent = x , x -> left = y;
+	}
+
+	void right_rotate(Node *x)
+	{
+		Node *y = x -> parent;
+		if (x -> right != nil) x -> right -> parent = y;
+		y -> left = x -> right;
+		if (y -> parent -> left == y) y -> parent -> left = x;
+		else y -> parent -> right = x;
+		x -> parent = y -> parent , y -> parent = x , x -> right = y;
+	}
+
+	void insert_fixup(Node *x)
+	{
+		for (Node *y;x -> parent -> color == RED;)
+			if (x -> parent -> parent -> left == x -> parent)
+			{
+				y = x -> parent -> parent -> right;
+				if (y -> color == RED)
+				{
+					x -> parent -> color = y -> color = BLACK , y -> parent -> color = RED;
+					x = y -> parent;
+				}
+				else
+				{
+					y = x -> parent;
+					if (y -> right == x) left_rotate(x) , x = y , y = x -> parent;
+					right_rotate(y) , swap(y -> color , y -> right -> color);
+				}
+			}
+			else
+			{
+				y = x -> parent -> parent -> left;
+				if (y -> color == RED)
+				{
+					x -> parent -> color = y -> color = BLACK , y -> parent -> color = RED;
+					x = y -> parent;
+				}
+				else
+				{
+					y = x -> parent;
+					if (y -> left == x) right_rotate(x) , x = y , y = x -> parent;
+					left_rotate(y) , swap(y -> color , y -> left -> color);
+				}
+			}
+		nil -> left -> color = BLACK;
+	}
+
+	void erase_fixup(Node *x)
+	{
+		for (Node *y;x -> parent != nil && x -> color == BLACK;)
+			if (x -> parent -> left == x)
+			{
+				y = x -> parent -> right;
+				if (y -> color == RED) left_rotate(y) , swap(y -> color , y -> left -> color);
+				else if (y -> left == BLACK && y -> right == BLACK)
+				{
+					x -> color = y -> color = RED , x -> parent -> color = BLACK;
+					x = x -> parent;
+				}
+				else
+				{
+					if (y -> right == BLACK)
+					{
+						right_rotate(y -> left) , swap(y -> color , y -> parent -> color);
+						y = y -> parent;
+					}
+					left_rotate(y) , swap(y -> color , y -> left -> color) , y -> right -> color = BLACK;
+					x = nil -> left;
+				}
+			}
+			else
+			{
+				y = x -> parent -> left;
+				if (y -> color == RED) right_rotate(y) , swap(y -> color , y -> right -> color);
+				else if (y -> left == BLACK && y -> right == BLACK)
+				{
+					x -> color = y -> color = RED , x -> parent -> color = BLACK;
+					x = x -> parent;
+				}
+				else
+				{
+					if (y -> left == BLACK)
+					{
+						left_rotate(y -> right) , swap(y -> color , y -> parent -> color);
+						y = y -> parent;
+					}
+					right_rotate(y) , swap(y -> color , y -> right -> color) , y -> left -> color = BLACK;
+					x = nil -> left;
+				}
+			}
+		x -> color = BLACK;
+	}
+
+	Node *findmin(const Node * const node) const
+	{
+		Node *x = node;
+		for (;x -> left != nil;x = x -> left);
+		return x;
+	}
+
+	Node *findmax(const Node * const node) const
+	{
+		Node *x = node;
+		for (;x -> right != nil;x = x -> right);
+		return x;
+	}
+
+	void transplant(Node * const x , Node * const y)
+	{
+		if (x == x -> parent -> left) x -> parent -> left = y;
+		else x -> parent -> right = y;
+		y -> parent = x -> parent;
+	}
+
+	Node *search(const Key &key) const
+	{
+		Node *x = nil -> left;
+		for (;x != nil && x -> key != key;x = compare(key , x -> key) ? x -> left : x -> right);
+		return x;
+	}
+
+	Node *insert(Node * const node)
+	{
+		Node *x = nil -> left , *y = nil;
+		for (;x != nil;y = x , x = compare(key , x -> key) ? x -> left : x -> right);
+		if (compare(key , y -> key)) y -> left = node;
+		else y -> right = node;
+		node -> color = RED , node -> left = node -> right = nil , node -> parent = y;
+		insert_fixup(x);
+	}
+
+	void erase(Node *x)
+	{
+		color_type col = x -> color;Node *y;
+		if (x -> left == nil) transplant(x , x -> right) , y = x -> right;
+		else if (x -> right == nil) transplant(x , x -> left) , y = x -> left;
+		else
+		{
+			Node *z = findmin(x -> right);col = z -> color , y = z -> right;
+			if (z -> parent == x) z -> right -> parent = z;
+			else
+			{
+				transplant(z , z -> right);
+				z -> right = x -> right , z -> right -> parent = z;
+			}
+			transplant(x , z);
+			z -> color = x -> color , z -> left = x -> left , z -> left -> parent = z;
+		}
+		delete x;
+		if (col == BLACK) erase_fixup(y);
+	}
 public:
 	/**
 	 * the internal type of data.
